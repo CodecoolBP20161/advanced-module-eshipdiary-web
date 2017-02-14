f208df1f4c864e4a8c33f1cf54316ae64a620c5b
Kristof Galla <gallakristof@gmail.com>
2017-02-14 22:28:43 +0100

    Remove code and generate it with Lombok instead

--- src/main/java/com/codecool/eshipdiary/model/User.java
+++ src/main/java/com/codecool/eshipdiary/model/User.java
@@ -1,11 +1,14 @@
 package com.codecool.eshipdiary.model;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@Data
 @Entity
 @Table(name = "`USER`")
 public class User {
@@ -55,118 +58,13 @@ public class User {
         ADMIN, USER
     } // separate class instead of enum based on spring security settings
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAPIKey() {
-        return APIKey;
-    }
-
-    public void setAPIKey(String APIKey) {
-        this.APIKey = APIKey;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
 
     public void setBirthDate(String birthDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         this.birthDate = Date.valueOf(LocalDate.parse(birthDate, formatter));
     }
 
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
 
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
-    public KnowledgeLevel getKnowledgeLevel() {
-        return knowledgeLevel;
-    }
-
-    public void setKnowledgeLevel(KnowledgeLevel knowledgeLevel) {
-        this.knowledgeLevel = knowledgeLevel;
-    }
-
-    public int getWeightInKg() {
-        return weightInKg;
-    }
-
-    public void setWeightInKg(int weightInKg) {
-        this.weightInKg = weightInKg;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", APIKey='" + APIKey + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", emailAddress='" + emailAddress + '\'' +
-                ", passwordHash='" + passwordHash + '\'' +
-                ", birthDate=" + birthDate +
-                ", phoneNumber=" + phoneNumber +
-                ", knowledgeLevel=" + knowledgeLevel +
-                ", weightInKg=" + weightInKg +
-                ", isActive=" + isActive +
-                '}';
-    }
 }
