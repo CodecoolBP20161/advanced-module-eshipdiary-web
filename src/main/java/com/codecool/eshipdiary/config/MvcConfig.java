ea23121d866fbd49412f3c09a1e0567fa1c2f2d7
eszterkaloz <eszterkaloz@gmail.com>
2017-02-15 14:02:04 +0100

    Config files moved to separate package

--- src/main/java/com/codecool/eshipdiary/config/MvcConfig.java
+++ src/main/java/com/codecool/eshipdiary/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.codecool.eshipdiary;
+package com.codecool.eshipdiary.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/com/codecool/eshipdiary/config/WebSecurityConfig.java
+++ src/main/java/com/codecool/eshipdiary/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.codecool.eshipdiary;
+package com.codecool.eshipdiary.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
