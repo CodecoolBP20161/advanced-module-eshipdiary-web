4940a0f113252de15d30dc69da103773e12145fd
eszterkaloz <eszterkaloz@gmail.com>
2017-02-15 14:09:10 +0100

    Additional methods for user database

--- src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
+++ src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.codecool.eshipdiary.repository;
 
 import com.codecool.eshipdiary.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findOneByEmail(String email);
+    Optional<User> findOneByUserName(String userName);
 }
