b496c7419daa7686acafbd58d2b114dc72025f38
György Hamar <gyurinak@gmail.com>
2017-02-19 20:37:51 +0100

    @JsonIgnore annotation to roles field

--- src/main/java/com/codecool/eshipdiary/model/User.java
+++ src/main/java/com/codecool/eshipdiary/model/User.java
@@ -63,6 +63,7 @@ public class User {
     @Column(nullable = false)
     private boolean isActive;
 
+    @JsonIgnore
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
