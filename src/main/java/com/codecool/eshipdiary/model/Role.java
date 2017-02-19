52e5cfdbe692d4b3ff0d83f6b0de46f429784923
eszterkaloz <eszterkaloz@gmail.com>
2017-02-19 16:06:49 +0100

    Model and repository for authorization (role)

---
+++ src/main/java/com/codecool/eshipdiary/model/Role.java
@@ -0,0 +1,23 @@
+package com.codecool.eshipdiary.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+}
---
+++ src/main/java/com/codecool/eshipdiary/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.eshipdiary.repository;
+
+import com.codecool.eshipdiary.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "role", path = "role")
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
