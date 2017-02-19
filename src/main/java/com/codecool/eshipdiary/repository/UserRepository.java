f5964944e89ee8477a2daf68d430b8ee91037e85
eszterkaloz <eszterkaloz@gmail.com>
2017-02-19 17:09:01 +0100

    Added annotation for collection resources

--- src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
+++ src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.codecool.eshipdiary.repository;
 
 import com.codecool.eshipdiary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByEmailAddress(String email);
     Optional<User> findOneByUserName(String userName);
