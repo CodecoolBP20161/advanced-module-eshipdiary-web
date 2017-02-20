e1dfdfed077e295b7f6536a2941be267ae681166
eszterkaloz <eszterkaloz@gmail.com>
2017-02-20 10:41:43 +0100

    Deleted unused import statements

--- src/main/java/com/codecool/eshipdiary/controller/HomeController.java
+++ src/main/java/com/codecool/eshipdiary/controller/HomeController.java
@@ -1,18 +1,7 @@
 package com.codecool.eshipdiary.controller;
 
-import com.codecool.eshipdiary.model.APIKeyValidator;
-import com.codecool.eshipdiary.repository.UserRepository;
-import com.codecool.eshipdiary.service.UserRepositoryService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestContextHolder;
 
 @Controller
 public class HomeController {
