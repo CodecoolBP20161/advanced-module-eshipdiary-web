b475bc98f37880791b6ce5b8fcf77b38e7ada52b
eszterkaloz <eszterkaloz@gmail.com>
2017-02-23 23:13:39 +0100

    Saving logic for new users

--- src/main/java/com/codecool/eshipdiary/controller/HomeController.java
+++ src/main/java/com/codecool/eshipdiary/controller/HomeController.java
@@ -1,12 +1,27 @@
 package com.codecool.eshipdiary.controller;
 
+import com.codecool.eshipdiary.model.User;
+import com.codecool.eshipdiary.service.UserRepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    UserRepositoryService userRepositoryService;
 
     @RequestMapping(value = "/")
     public String index() {
@@ -18,8 +33,23 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/users")
-    public String getUserTable() {
+    public String getUserTable(Model model) {
+        model.addAttribute("newUser", new User());
         return "users";
     }
+
+    @RequestMapping(value = "/users" , method = RequestMethod.POST)
+    public String saveNewUser(@Valid @ModelAttribute("newUser") User newUser, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // todo: frontend input error handling
+            LOG.error("Error while trying to create a new user", bindingResult);
+            return "users";
+        } else {
+            LOG.info("New user: ", newUser);
+            userRepositoryService.create(newUser);
+            return "users";
+        }
+    }
+
 
 }
