855b13f44b495e46fc368d4e23c743e270c41ec1
eszterkaloz <eszterkaloz@gmail.com>
2017-03-01 12:20:31 +0100

    Trying to catch ConstraintViolationEx separately

--- src/main/java/com/codecool/eshipdiary/controller/UserController.java
+++ src/main/java/com/codecool/eshipdiary/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.sql.SQLException;
@@ -38,7 +39,8 @@ public class UserController {
 
     @RequestMapping(value = "user", method = RequestMethod.POST)
     public
-    @ResponseBody ValidationResponse saveNewUser(@ModelAttribute("user") @Valid User newUser, BindingResult result) {
+    @ResponseBody
+    ValidationResponse saveNewUser(@ModelAttribute("user") @Valid User newUser, BindingResult result) {
         ValidationResponse response = new ValidationResponse();
 
         if (!result.hasErrors()) {
@@ -47,15 +49,24 @@ public class UserController {
 
         try {
             userRepositoryService.create(newUser);
-        } catch (DataIntegrityViolationException | ConstraintViolationException | SQLException e) {
+        } catch (ConstraintViolationException e) {
+            response.setStatus("FAIL");
+            StringBuilder sb = new StringBuilder();
+
+            for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+                LOG.error("Error: " + violation.getPropertyPath() + ":" + violation.getInvalidValue() + " : " + violation.getMessage() + "\n");
+                sb.append(violation.getMessage());
+            }
+            response.setResult(sb);
+        } catch (DataIntegrityViolationException | SQLException e) {
             response.setStatus("FAIL");
             response.setResult("Nem várt hiba: " + e.getMessage());
-            LOG.error("Could not save new user", e.getMessage());
+            LOG.error("Could not save new user {}", e.getMessage());
 
-            if(userRepositoryService.getUserByUserName(newUser.getUserName()).isPresent()) {
+            if (userRepositoryService.getUserByUserName(newUser.getUserName()).isPresent()) {
                 response.setResult("Foglalt felhasználónév");
             }
-            if(userRepositoryService.getUserByEmail(newUser.getEmailAddress()).isPresent()) {
+            if (userRepositoryService.getUserByEmail(newUser.getEmailAddress()).isPresent()) {
                 response.setResult("A megadott email-címmel már van regisztrált tag");
             }
         }
