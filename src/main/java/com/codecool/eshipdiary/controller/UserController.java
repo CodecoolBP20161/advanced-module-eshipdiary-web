522a6c28a43582157a37a85f1ec03bd1732e1f76
eszterkaloz <eszterkaloz@gmail.com>
2017-03-02 19:29:24 +0100

    Create operation for userCRUD

--- src/main/java/com/codecool/eshipdiary/controller/UserController.java
+++ src/main/java/com/codecool/eshipdiary/controller/UserController.java
@@ -8,8 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -27,6 +31,20 @@ public class UserController {
     @RequestMapping("/users")
     public String getUserTable(Model model) {
         model.addAttribute("user");
+        return "users";
+    }
+
+    @RequestMapping(value = "users/new", method = RequestMethod.GET)
+    public String userForm() {
+        return "user_form";
+    }
+
+    @RequestMapping(value = "/users/new", method = RequestMethod.POST)
+    public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult result) {
+        if(result.hasErrors()) {
+            LOG.info("result" + result.getFieldErrors());
+            return "user_form";
+        }
         return "users";
     }
     
