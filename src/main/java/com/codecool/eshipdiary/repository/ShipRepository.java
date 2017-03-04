4dcd9e127b83e2c767502837e7fca6ec11756931
eszterkaloz <eszterkaloz@gmail.com>
2017-03-04 09:56:29 +0100

    Repository and service for ship

---
+++ src/main/java/com/codecool/eshipdiary/repository/ShipRepository.java
@@ -0,0 +1,17 @@
+package com.codecool.eshipdiary.repository;
+
+
+import com.codecool.eshipdiary.model.Ship;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "ship", path = "ship")
+public interface ShipRepository extends CrudRepository<Ship, Long> {
+    Optional<Ship> findOneByName(String name);
+    Optional<Ship> findOneByShipType(String shipType);
+    Optional<Ship> findOneByCategory(String category);
+}
