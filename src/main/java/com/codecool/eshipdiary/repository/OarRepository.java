519474a82c3bbf108cfa8f48b27d898130b4b27d
Balazs Ando <elnesia@gmail.com>
2017-03-05 17:55:32 +0100

    remove unnecessary imports

--- src/main/java/com/codecool/eshipdiary/repository/OarRepository.java
+++ src/main/java/com/codecool/eshipdiary/repository/OarRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 @RepositoryRestResource(collectionResourceRel = "oar", path = "oar")
 public interface OarRepository extends CrudRepository<Oar, Long> {
--- src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
+++ src/main/java/com/codecool/eshipdiary/repository/UserRepository.java
@@ -1,19 +1,15 @@
 package com.codecool.eshipdiary.repository;
 
 import com.codecool.eshipdiary.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findOneByEmailAddress(String email);
     Optional<User> findOneByUserName(String userName);
     Optional<User> findOneByApiKey(String APIKey);
 }
