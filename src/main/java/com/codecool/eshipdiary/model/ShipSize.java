3d65e7fb0e196d2f6eec979ee1a75b60ecc147da
Balazs Ando <elnesia@gmail.com>
2017-03-08 18:35:53 +0100

    hotfix shipsize model

--- src/main/java/com/codecool/eshipdiary/model/ShipSize.java
+++ src/main/java/com/codecool/eshipdiary/model/ShipSize.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -20,10 +21,12 @@ public class ShipSize {
     private String name;
 
     @NotNull(message = "A mező nem lehet üres")
+    @Min(message = "Nem lehet kisebb, mint egy", value = 1)
     @Column(nullable = false)
-    private String minKg;
+    private int minKg;
 
     @NotNull(message = "A mező nem lehet üres")
+    @Min(message = "Nem lehet kisebb, mint egy", value = 1)
     @Column(nullable = false)
-    private String maxKg;
+    private int maxKg;
 }
