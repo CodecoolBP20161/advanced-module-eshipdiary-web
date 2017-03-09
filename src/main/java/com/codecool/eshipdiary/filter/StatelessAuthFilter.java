d106ca1065fc9376331578e397289b81c0307286
György Hamar <gyurinak@gmail.com>
2017-03-09 17:43:14 +0100

    token based filter

--- src/main/java/com/codecool/eshipdiary/filter/StatelessAuthFilter.java
+++ src/main/java/com/codecool/eshipdiary/filter/StatelessAuthFilter.java
@@ -1,9 +1,14 @@
 package com.codecool.eshipdiary.filter;
 
 import com.codecool.eshipdiary.service.ApiAuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -16,7 +21,9 @@ import java.io.IOException;
 /**
  * Created by hamargyuri on 2017. 02. 28..
  */
+@Component
 public class StatelessAuthFilter extends GenericFilterBean {
+    private static final Logger LOG = LoggerFactory.getLogger(StatelessAuthFilter.class);
 
     @Autowired
     ApiAuthenticationService apiAuthenticationService;
@@ -25,9 +32,18 @@ public class StatelessAuthFilter extends GenericFilterBean {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        Authentication authentication = apiAuthenticationService.getAuth(httpServletRequest);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication;
+
+        try {
+            authentication = apiAuthenticationService.getAuth(httpServletRequest);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            LOG.info("api authentication successful: {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+        } catch (InsufficientAuthenticationException invalidToken) {
+            LOG.info("api auth failed {}", invalidToken.getMessage());
+        } catch (AuthenticationCredentialsNotFoundException noToken) {
+            LOG.debug("{}, moving on to next authentication link", noToken.getMessage());
+        }
         filterChain.doFilter(servletRequest, servletResponse);
-        SecurityContextHolder.getContext().setAuthentication(null);
     }
 }
