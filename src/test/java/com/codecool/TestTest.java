7f346b1b280263623b3ab12f094f27e2d53e8c00
György Hamar <gyurinak@gmail.com>
2017-03-16 14:56:48 +0100

    travis settings updated

---
+++ src/test/java/com/codecool/TestTest.java
@@ -0,0 +1,14 @@
+package com.codecool;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by hamargyuri on 2017. 03. 16..
+ */
+public class TestTest {
+    @Test
+    public void testTest() {
+        assertEquals(true, false);
+    }
+}
