3181344ccd97e6a9e9a2f71dbc2f8ebc1c58adca
eszterkaloz <eszterkaloz@gmail.com>
2017-03-17 16:44:10 +0100

    Customized multiple select with display

--- src/main/resources/static/js/rental_form.js
+++ src/main/resources/static/js/rental_form.js
@@ -1,10 +1,47 @@
 $(document).ready(function() {
+    simpleSelectWithFilter();
+    multipleSelect();
+});
+
+
+var simpleSelectWithFilter = function () {
     $("#chosenShip").multiselect({
         enableCaseInsensitiveFiltering: true,
-        filterPlaceholder: 'Válassz hajót...'
-    });
-    $('.multi-select').multiselect({
+        filterPlaceholder: 'Keresés...',
+        buttonWidth: '368px'
+    })
+};
+
+var multipleSelect = function () {
+    var crewList = [];
+    $('#crew').multiselect({
         enableCaseInsensitiveFiltering: true,
-        filterPlaceholder: 'Válassz legénységet...'
+        filterPlaceholder: 'Keresés...',
+        buttonText: function(options, select) {
+            return 'Válassz legénységet!';
+        },
+        buttonWidth: '368px',
+        onChange: function(element, checked) {
+            if (checked === true) {
+                crewList.push(element.html());
+                document.getElementById("crewDisplay").innerHTML = displayCrew(crewList);
+            }
+        }
     });
-});
+    $('#oars').multiselect({
+        enableCaseInsensitiveFiltering: true,
+        filterPlaceholder: 'Keresés...',
+        buttonText: function (options, select) {
+            return 'Válassz lapátokat!';
+        },
+        buttonWidth: '368px'
+    });
+};
+
+var displayCrew = function (crewList) {
+    var text = "<ul>";
+    for (var i = 0; i < crewList.length; i++) {
+        text += "<li>" + crewList[i] + "</li>";
+    }
+    return text;
+};
