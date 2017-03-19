fe9a8fddd03c9097afcaba891664462b878b814f
eszterkaloz <eszterkaloz@gmail.com>
2017-03-19 16:32:02 +0100

    Saving rentalLog to db

--- src/main/java/com/codecool/eshipdiary/controller/RentalController.java
+++ src/main/java/com/codecool/eshipdiary/controller/RentalController.java
@@ -1,24 +1,27 @@
 package com.codecool.eshipdiary.controller;
 
-import com.codecool.eshipdiary.model.Oar;
-import com.codecool.eshipdiary.model.RentalLog;
-import com.codecool.eshipdiary.model.Ship;
-import com.codecool.eshipdiary.model.User;
-import com.codecool.eshipdiary.service.OarRepositoryService;
-import com.codecool.eshipdiary.service.RentalLogRepositoryService;
-import com.codecool.eshipdiary.service.ShipRepositoryService;
-import com.codecool.eshipdiary.service.UserRepositoryService;
+import com.codecool.eshipdiary.model.*;
+import com.codecool.eshipdiary.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
 public class RentalController {
+    private static final Logger LOG = LoggerFactory.getLogger(RentalController.class);
+
+    @Autowired
+    private RentalLogRepositoryService rentalLogRepositoryService;
 
     @Autowired
     private UserRepositoryService userRepositoryService;
@@ -27,11 +30,18 @@ public class RentalController {
     private ShipRepositoryService shipRepositoryService;
 
     @Autowired
+    private ShipTypeRepositoryService shipTypeRepositoryService;
+
+    @Autowired
     private OarRepositoryService oarRepositoryService;
 
     @ModelAttribute("rental")
     public RentalLog rentalLog() {
-        return new RentalLog();
+        RentalLog rentalLog = new RentalLog();
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> user = userRepositoryService.getUserByUserName(userName);
+        rentalLog.setCaptain(user.isPresent() ? user.get() : null);
+        return rentalLog;
     }
 
     @ModelAttribute("users")
@@ -44,6 +54,11 @@ public class RentalController {
         return (List<Ship>) shipRepositoryService.getAllShips();
     }
 
+    @ModelAttribute("shipTypes")
+    public List<ShipType> listShipTypes() {
+        return (List<ShipType>) shipTypeRepositoryService.getAllShipType();
+    }
+
     @ModelAttribute("oars")
     public List<Oar> listOars() {
         return (List<Oar>) oarRepositoryService.getAllOars();
@@ -54,7 +69,28 @@ public class RentalController {
         model.addAttribute("rental");
         model.addAttribute("users");
         model.addAttribute("ships");
+        model.addAttribute("shipTypes");
         model.addAttribute("oars");
         return "rentals";
+    }
+
+    @RequestMapping(value = "/rentals", method = RequestMethod.OPTIONS)
+    public String saveOrUpdateRentalForm(Model model){
+        LOG.info("Saving rental form");
+        model.addAttribute("rental");
+        model.addAttribute("validate", "return validateRentalLog()");
+        return "rental_log/rental_form";
+    }
+
+    @RequestMapping(value = "rentals", method = RequestMethod.POST)
+    public String updateShip(@ModelAttribute("rental") @Valid RentalLog rentalLog, BindingResult result, Model model) {
+        model.addAttribute("validate", "return validateRentalLog()");
+
+        if(result.hasErrors()) {
+            LOG.error("Error while trying to update rentalLog: " + result.getFieldErrors());
+        } else {
+            model.addAttribute("submit", "return submitRentalLog()");
+        }
+        return "rental_log/rental_form";
     }
 }
