3c11ed6f3bd83c00cbdf299f77f9f574b23cd151
György Hamar <gyurinak@gmail.com>
2017-03-20 11:38:59 +0100

    changed to mockMvc testing and added indexWithNotAdmin testcase

--- src/test/java/com/codecool/eshipdiary/controller/HomeControllerTest.java
+++ src/test/java/com/codecool/eshipdiary/controller/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.codecool.eshipdiary.controller;
 
+import com.codecool.MockAuthentication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,21 +11,19 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.*;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by hamargyuri on 2017. 03. 19..
@@ -41,60 +40,33 @@ public class HomeControllerTest {
 
     private MockMvc mockMvc;
 
-    private Authentication authentication;
+    private HashSet<GrantedAuthority> authorities;
+
+    private MockAuthentication authentication;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
 
         PowerMockito.mockStatic(SecurityContextHolder.class);
         BDDMockito.given(SecurityContextHolder.getContext()).willReturn(securityContext);
-
+        authorities = new HashSet<>();
     }
 
     @Test
     public void indexWithAdmin() throws Exception {
-        authentication = new Authentication() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                Collection<GrantedAuthority> authorities = new HashSet<>();
-                authorities.add(new SimpleGrantedAuthority("ADMIN"));
-                return authorities;
-            }
-
-            @Override
-            public Object getCredentials() {
-                return null;
-            }
-
-            @Override
-            public Object getDetails() {
-                return null;
-            }
-
-            @Override
-            public Object getPrincipal() {
-                return null;
-            }
-
-            @Override
-            public boolean isAuthenticated() {
-                return false;
-            }
-
-            @Override
-            public void setAuthenticated(boolean b) throws IllegalArgumentException {
-
-            }
-
-            @Override
-            public String getName() {
-                return null;
-            }
-        };
+        authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        authentication = new MockAuthentication(authorities);
         when(securityContext.getAuthentication()).thenReturn(authentication);
-        assertEquals("redirect:/users", homeController.index());
+        mockMvc.perform(get("/")).andExpect(redirectedUrl("/users"));
     }
 
+    @Test
+    public void indexWithNotAdmin() throws Exception {
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        authentication = new MockAuthentication(authorities);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        mockMvc.perform(get("/")).andExpect(forwardedUrl("index"));
+    }
 }
