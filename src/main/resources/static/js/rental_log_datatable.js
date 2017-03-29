9fb34b13d8cab9c8ccfba7e0b1123ab14caea61a
Kristof Galla <gallakristof@gmail.com>
2017-03-29 12:30:44 +0200

    Improve ellipsis function

--- src/main/resources/static/js/rental_log_datatable.js
+++ src/main/resources/static/js/rental_log_datatable.js
@@ -28,7 +28,7 @@ $(document).ready( function () {
             },
             {
                 data: 'comment',
-                render: $.fn.dataTable.render.ellipsis(10)
+                render: $.fn.dataTable.render.ellipsis(15, true, false)
             },
             {
                 sortable: false,
@@ -218,10 +218,44 @@ function selectShipsByName() {
     })
 }
 
-$.fn.dataTable.render.ellipsis = function ( cutoff ) {
-    return function ( data, type, row ) {
-        return type === 'display' && data.length > cutoff ?
-            data.substr( 0, cutoff ) +'…' :
-            data;
-    }
+$.fn.dataTable.render.ellipsis = function ( cutoff, wordbreak, escapeHtml ) {
+    var esc = function ( t ) {
+        return t
+            .replace( /&/g, '&amp;' )
+            .replace( /</g, '&lt;' )
+            .replace( />/g, '&gt;' )
+            .replace( /"/g, '&quot;' );
+    };
+
+    return function ( d, type, row ) {
+        // Order, search and type get the original data
+        if ( type !== 'display' ) {
+            return d;
+        }
+
+        if ( typeof d !== 'number' && typeof d !== 'string' ) {
+            console.log("the type of the data inserted to ellipsis function is not number or string")
+            return d;
+        }
+
+        d = d.toString(); // cast numbers
+
+        if ( d.length < cutoff ) {
+            return d;
+        }
+
+        var shortened = d.substr(0, cutoff-1);
+
+        // Find the last white space character in the string
+        if ( wordbreak ) {
+            shortened = shortened.replace(/\s([^\s]*)$/, '');
+        }
+
+        // Protect against uncontrolled HTML input
+        if ( escapeHtml ) {
+            shortened = esc( shortened );
+        }
+
+        return '<span class="ellipsis" title="'+esc(d)+'">'+shortened+'&#8230;</span>';
+    };
 };
