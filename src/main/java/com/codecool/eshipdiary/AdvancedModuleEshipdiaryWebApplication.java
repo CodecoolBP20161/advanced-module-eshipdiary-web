814e5154d7a582ce77bc89cac9ceb67aa9a865ba
György Hamar <gyurinak@gmail.com>
2017-04-03 14:26:49 +0200

    Adding a digusting, but hopefullyworking solution to avoid db duplications

--- src/main/java/com/codecool/eshipdiary/AdvancedModuleEshipdiaryWebApplication.java
+++ src/main/java/com/codecool/eshipdiary/AdvancedModuleEshipdiaryWebApplication.java
@@ -25,89 +25,92 @@ public class AdvancedModuleEshipdiaryWebApplication {
     @Bean
     public CommandLineRunner demo(UserRepository userRepository, ClubRepository clubRepository, ShipRepository shipRepository, RoleRepository roleRepository, OarRepository oarRepository, ShipTypeRepository shipTypeRepository) {
         return (args) -> {
+            LOG.info("rolerepository: {}", roleRepository.findAll().iterator().hasNext());
 
-            Role admin = new Role();
-            admin.setName("ADMIN");
-            roleRepository.save(admin);
+            if (!roleRepository.findAll().iterator().hasNext()) {
 
-            Club bee = new Club();
-            bee.setName("BEE");
-            clubRepository.save(bee);
+                Role admin = new Role();
+                admin.setName("ADMIN");
+                roleRepository.save(admin);
 
-            Club dnhe = new Club();
-            dnhe.setName("DNHE");
-            clubRepository.save(dnhe);
+                Role userRole = new Role();
+                userRole.setName("USER");
+                roleRepository.save(userRole);
 
-            User ex = new User();
-            ex.setFirstName("bee");
-            ex.setLastName("admin");
-            ex.setUserName("admin");
-            ex.setBirthDate(Date.valueOf("1989-01-01"));
-            ex.setActive(true);
-            ex.setEmailAddress("admin@yahoo.com");
-            ex.setPasswordHash("password");
-            ex.setPhoneNumber("22021043");
-            ex.setKnowledgeLevel(User.KnowledgeLevel.BEGINNER);
-            ex.setWeightInKg(80);
-            ex.setRole(admin);
-            ex.setClub(bee);
-            userRepository.save(ex);
+                Club bee = new Club();
+                bee.setName("BEE");
+                clubRepository.save(bee);
 
-            User dek = new User();
-            dek.setFirstName("dnhe");
-            dek.setLastName("admin");
-            dek.setUserName("admin2");
-            dek.setBirthDate(Date.valueOf("1989-01-01"));
-            dek.setActive(true);
-            dek.setEmailAddress("admin2@yahoo.com");
-            dek.setPasswordHash("password");
-            dek.setPhoneNumber("22021043");
-            dek.setKnowledgeLevel(User.KnowledgeLevel.BEGINNER);
-            dek.setWeightInKg(80);
-            dek.setRole(admin);
-            dek.setClub(dnhe);
-            userRepository.save(dek);
+                Club dnhe = new Club();
+                dnhe.setName("DNHE");
+                clubRepository.save(dnhe);
 
+                User ex = new User();
+                ex.setFirstName("bee");
+                ex.setLastName("admin");
+                ex.setUserName("admin");
+                ex.setBirthDate(Date.valueOf("1989-01-01"));
+                ex.setActive(true);
+                ex.setEmailAddress("admin@yahoo.com");
+                ex.setPasswordHash("password");
+                ex.setPhoneNumber("22021043");
+                ex.setKnowledgeLevel(User.KnowledgeLevel.BEGINNER);
+                ex.setWeightInKg(80);
+                ex.setRole(admin);
+                ex.setClub(bee);
+                userRepository.save(ex);
 
-            Role userRole = new Role();
-            userRole.setName("USER");
-            roleRepository.save(userRole);
+                User dek = new User();
+                dek.setFirstName("dnhe");
+                dek.setLastName("admin");
+                dek.setUserName("admin2");
+                dek.setBirthDate(Date.valueOf("1989-01-01"));
+                dek.setActive(true);
+                dek.setEmailAddress("admin2@yahoo.com");
+                dek.setPasswordHash("password");
+                dek.setPhoneNumber("22021043");
+                dek.setKnowledgeLevel(User.KnowledgeLevel.BEGINNER);
+                dek.setWeightInKg(80);
+                dek.setRole(admin);
+                dek.setClub(dnhe);
+                userRepository.save(dek);
 
-            User user = new User();
-            user.setFirstName("first");
-            user.setLastName("user");
-            user.setUserName("user");
-            user.setBirthDate(Date.valueOf("1900-01-01"));
-            user.setActive(true);
-            user.setEmailAddress("user@yahoo.com");
-            user.setPasswordHash("");
-            user.setPhoneNumber("62426180");
-            user.setKnowledgeLevel(User.KnowledgeLevel.ADVANCED);
-            user.setWeightInKg(90);
-            user.setRole(userRole);
-            user.setClub(bee);
-            userRepository.save(user);
+                User user = new User();
+                user.setFirstName("first");
+                user.setLastName("user");
+                user.setUserName("user");
+                user.setBirthDate(Date.valueOf("1900-01-01"));
+                user.setActive(true);
+                user.setEmailAddress("user@yahoo.com");
+                user.setPasswordHash("");
+                user.setPhoneNumber("62426180");
+                user.setKnowledgeLevel(User.KnowledgeLevel.ADVANCED);
+                user.setWeightInKg(90);
+                user.setRole(userRole);
+                user.setClub(bee);
+                userRepository.save(user);
 
-            ShipType shipType = new ShipType();
-            shipType.setName("Kayak");
-            shipType.setClub(bee);
-            shipTypeRepository.save(shipType);
+                ShipType shipType = new ShipType();
+                shipType.setName("Kayak");
+                shipType.setClub(bee);
+                shipTypeRepository.save(shipType);
 
-            Ship ship = new Ship();
-            ship.setName("Atlanta");
-            ship.setCategory(Ship.Category.TRAINING);
-            ship.setCode("4x+");
-            ship.setCoxed(true);
-            ship.setMaxSeat(4);
-            ship.setType(shipType);
-            ship.setClub(bee);
-            shipRepository.save(ship);
+                Ship ship = new Ship();
+                ship.setName("Atlanta");
+                ship.setCategory(Ship.Category.TRAINING);
+                ship.setCode("4x+");
+                ship.setCoxed(true);
+                ship.setMaxSeat(4);
+                ship.setType(shipType);
+                ship.setClub(bee);
+                shipRepository.save(ship);
 
-            Oar oar = new Oar();
-            oar.setName("testOar");
-            oar.setType(shipType);
-            oar.setClub(bee);
-            oarRepository.save(oar);
+                Oar oar = new Oar();
+                oar.setName("testOar");
+                oar.setType(shipType);
+                oar.setClub(bee);
+                oarRepository.save(oar);
+            }
         };
     }
 }
