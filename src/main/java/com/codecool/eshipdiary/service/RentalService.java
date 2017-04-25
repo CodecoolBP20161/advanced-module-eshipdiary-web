5c34a1b600bbe1cc07a5dd96cf968c8fc26f6bc0
György Hamar <gyurinak@gmail.com>
2017-04-25 11:48:54 +0200

    added ...RepositoryService.save() methods to setOnWater...() and addedsaveIfItemsAreAvailable()

--- src/main/java/com/codecool/eshipdiary/service/RentalService.java
+++ src/main/java/com/codecool/eshipdiary/service/RentalService.java
@@ -1,9 +1,13 @@
 package com.codecool.eshipdiary.service;
 
+import com.codecool.eshipdiary.controller.RentalController;
+import com.codecool.eshipdiary.exception.RentalCannotBeSaved;
 import com.codecool.eshipdiary.model.Oar;
 import com.codecool.eshipdiary.model.RentalLog;
 import com.codecool.eshipdiary.model.Ship;
 import com.codecool.eshipdiary.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,11 @@ import java.util.List;
 
 @Service
 public class RentalService {
+    private static final Logger LOG = LoggerFactory.getLogger(RentalService.class);
+
+    @Autowired
+    RentalLogRepositoryService rentalLogRepositoryService;
+
     @Autowired
     ShipRepositoryService shipRepositoryService;
 
@@ -21,17 +30,36 @@ public class RentalService {
     @Autowired
     UserRepositoryService userRepositoryService;
 
+    public void saveIfItemsAreAvailable(RentalLog rentalLog) throws RentalCannotBeSaved {
+        boolean everythingIsAvailable;
+        everythingIsAvailable = !rentalLog.getChosenShip().isOnWater();
+        for (Oar oar : rentalLog.getOars()) {
+            if (oar.isOnWater()) everythingIsAvailable = false;
+        }
+        for (User user : rentalLog.getCrew()) {
+            if (user.isOnWater()) everythingIsAvailable = false;
+        }
+        if (everythingIsAvailable) {
+            rentalLogRepositoryService.save(rentalLog);
+            setOnWaterForInvolvedItemsIn(rentalLog, true);
+        }
+        else throw new RentalCannotBeSaved("Some of the items to save are in another active rental.");
+    }
 
-    public void setOnWaterForInvolvedItemsIn(RentalLog log, Boolean bool){
+    void setOnWaterForInvolvedItemsIn(RentalLog log, Boolean bool){
         log.getChosenShip().setOnWater(bool);
+        shipRepositoryService.save(log.getChosenShip());
         if (log.getCox() != null) {
             log.getCox().setOnWater(bool);
+            userRepositoryService.save(log.getCox());
         }
         for (Oar oar : log.getOars()) {
             oar.setOnWater(bool);
+            oarRepositoryService.save(oar);
         }
         for (User user : log.getCrew()) {
             user.setOnWater(bool);
+            userRepositoryService.save(user);
         }
     }
 
@@ -51,7 +79,5 @@ public class RentalService {
                 newLog.setCox(oldLog.getCox());
             }
         }
-
-
     }
 }
